7737907bcd26d8254bd986ef8834d86c57eed86c
KimRass <rmx1000@yonsei.ac.kr>
2023-02-05 23:05:07 +0900

    colornet, stefann

--- colornet.py
+++ colornet.py
@@ -122,69 +122,6 @@ class DataGenerator(object):
             yield [[x1, x2], y1]
 
 
-# Colornet class
-class Colornet(object):
-    def __new__(self, input_shapes, optimizer, loss, weights=None):
-        # build network
-        x1 = Input(input_shapes[0])
-        x2 = Input(input_shapes[1])
-        
-        y1 = Conv2D(filters=64, kernel_size=(3, 3), padding='same')(x1)
-        y1 = LeakyReLU(alpha=0.2)(y1)
-        y1 = BatchNormalization()(y1)
-        
-        y2 = Conv2D(filters=64, kernel_size=(3, 3), padding='same')(x2)
-        y2 = LeakyReLU(alpha=0.2)(y2)
-        y2 = BatchNormalization()(y2)
-        
-        y = Concatenate()([y1, y2])
-        
-        y = Conv2D(filters=64, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        y = Conv2D(filters=64, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        
-        y = MaxPooling2D(pool_size=(2, 2))(y)
-        
-        y = Conv2D(filters=128, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        y = Conv2D(filters=128, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        
-        y = MaxPooling2D(pool_size=(2, 2))(y)
-        
-        y = Conv2D(filters=256, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        y = Conv2D(filters=256, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        y = Conv2D(filters=256, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        
-        y = UpSampling2D(size=(2, 2))(y)
-        y = Conv2D(filters=128, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        
-        y = UpSampling2D(size=(2, 2))(y)
-        y = Conv2D(filters=64, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        
-        y = Conv2D(filters=3, kernel_size=(3, 3), padding='same')(y)
-        y = LeakyReLU(alpha=0.2)(y)
-        
-        # compile network
-        model = Model(inputs=[x1, x2], outputs=y)
-        model.compile(optimizer=optimizer, loss=loss)
-        
-        # optionally load existing weights into network
-        try:
-            if not weights is None:
-                model.load_weights(weights)
-        except:
-            pass
-        
-        return model
-
-
 # ProgressMonitor class
 class ProgressMonitor(Callback):
     def __init__(self, image_dir_input1, image_dir_input2, image_dir_output,
